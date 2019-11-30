03c5639742f0ee6f7d0a42c04b53f4212d3563d3
Mark Wahlbeck <mark@markwahlbeck.com>
2019-11-29 19:43:32 -0600

    updated project, made change notes in 5:35.md file

--- section-5-chapter-35-in-app-purchases/InAppPurchases/AppDelegate.swift
+++ section-5-chapter-35-in-app-purchases/InAppPurchases/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 
@@ -40,7 +39,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
