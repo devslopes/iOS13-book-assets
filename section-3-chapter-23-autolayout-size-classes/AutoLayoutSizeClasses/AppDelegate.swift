e9ee75b6b08d7065e2a1f834ec5338cc910c0ad4
nate_the_coder <nathanbrewer88@yahoo.com>
2019-12-05 19:18:28 -0600

    Update AppDelegate.swift

--- section-3-chapter-23-autolayout-size-classes/AutoLayoutSizeClasses/AppDelegate.swift
+++ section-3-chapter-23-autolayout-size-classes/AutoLayoutSizeClasses/AppDelegate.swift
@@ -1,10 +1,6 @@
 //
 //  AppDelegate.swift
 //  AutoLayoutSizeClasses
-//
-//  Created by Caleb Stultz on 10/22/16.
-//  Copyright © 2016 Caleb Stultz. All rights reserved.
-//
 
 import UIKit
 
@@ -12,7 +8,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
