f9d7c1385982f1039e45d7ec46b8ea7c0bb0ffd2
nate_the_coder <nathanbrewer88@yahoo.com>
2019-12-05 19:19:08 -0600

    Update ViewController.swift

--- section-3-chapter-23-autolayout-size-classes/AutoLayoutSizeClasses/ViewController.swift
+++ section-3-chapter-23-autolayout-size-classes/AutoLayoutSizeClasses/ViewController.swift
@@ -1,10 +1,6 @@
 //
 //  ViewController.swift
 //  AutoLayoutSizeClasses
-//
-//  Created by Caleb Stultz on 10/22/16.
-//  Copyright © 2016 Caleb Stultz. All rights reserved.
-//
 
 import UIKit
 
@@ -20,6 +16,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
