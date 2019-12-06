8156c5c58e9d5ba0d22dfe05d9588ebd904ab131
nate_the_coder <nathanbrewer88@yahoo.com>
2019-12-05 20:47:13 -0600

    Update ViewController.swift

--- section-2-chapter-20-uiscrollview/PageTheScroll/ViewController.swift
+++ section-2-chapter-20-uiscrollview/PageTheScroll/ViewController.swift
@@ -1,10 +1,6 @@
 //
 //  ViewController.swift
 //  PageTheScroll
-//
-//  Created by Mark Price on 7/10/16.
-//  Copyright © 2016 Devslopes. All rights reserved.
-//
 
 import UIKit
 
@@ -27,21 +23,17 @@ class ViewController: UIViewController {
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
-            
             var newX: CGFloat = 0.0
             
             newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
-            
             contentWidth += newX
             
             scrollView.addSubview(imageView)
-            
             imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.height / 2) - 75, width: 150, height: 150)
             
         }
         
         scrollView.clipsToBounds = false
-        
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
     }
 
